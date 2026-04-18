07eade21e4051b4ad30fa4c60a21d674c193f7ac
Vicky7999 <vigneshwar6208@gmail.com>
2026-04-19 01:27:22 +0530

    fixed cloudinary config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
